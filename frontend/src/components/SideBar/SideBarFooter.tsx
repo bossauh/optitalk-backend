37b91d0427f6de703f146fd5d6c02cc852674b2a
Philippe Mathew <philmattdev@gmail.com>
2023-05-21 08:22:20 +0800

    feat: add donation button

--- frontend/src/components/SideBar/SideBarFooter.tsx
+++ frontend/src/components/SideBar/SideBarFooter.tsx
@@ -67,6 +67,17 @@ const SideBarFooter: FC = () => {
           Sign Up
         </Button>
       )}
+      <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <input type="hidden" name="hosted_button_id" value="A4U2HUGYFZTSL" />
+        <input
+          type="image"
+          src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+          name="submit"
+          title="PayPal - The safer, easier way to pay online!"
+          alt="Donate with PayPal button"
+        />
+        <img alt="" src="https://www.paypal.com/en_PH/i/scr/pixel.gif" width="1" height="1" />
+      </form>
     </Card.Footer>
   );
 };

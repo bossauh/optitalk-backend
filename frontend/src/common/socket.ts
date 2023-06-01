ea4737fcab96eb393b964aaec0a71ca2d6f52c92
Philippe Mathew <philmattdev@gmail.com>
2023-06-01 15:05:24 +0800

    feat: update socket.io url

--- frontend/src/common/socket.ts
+++ frontend/src/common/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.NODE_ENV === "development" ? "http://127.0.0.1:5000" : "http://optitalk.net");
+const socket = io(process.env.NODE_ENV === "development" ? "http://127.0.0.1:5000" : "https://optitalk.net");
 
 export default socket;

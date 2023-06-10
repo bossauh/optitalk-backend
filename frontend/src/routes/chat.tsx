3f57b4b8fe4d56117d9cb7e54913b40dec7be3cd
Philippe Mathew <philmattdev@gmail.com>
2023-06-10 11:26:35 +0800

    fix: 150 messages to 1,000 messages text fix

--- frontend/src/routes/chat.tsx
+++ frontend/src/routes/chat.tsx
@@ -59,7 +59,7 @@ const Chat: FC = () => {
           let message: string;
           if (!storeCtx?.authenticated) {
             message =
-              "Oops! It seems like you've hit the limit of 20 messages per month on your unregistered account. I did this to reduce spam. You can login and get 150 more messages. Additionally, subscription plans are also available [here](https://www.buymeacoffee.com/philippemathew/membership) which provides up to thousands of messages, it is also a way for you to support me and the development of this project as this can get quite expensive to operate.";
+              "Oops! It seems like you've hit the limit of 20 messages per month on your unregistered account. I did this to reduce spam. You can login and get 1,000 more messages. Additionally, donator plans are also available [here](https://www.buymeacoffee.com/philippemathew/membership) which provides up to thousands of messages, it is also a way for you to support me and the development of this project as this can get quite expensive to operate.";
           } else {
             message = `Oh no! It seems you've reached your current limit, which allows for a maximum of ${d.payload.limit} messages per month. You can [pick a subscription plan here](https://www.buymeacoffee.com/philippemathew/membership) in order to get more messages as well as support me and the development of this project.`;
           }

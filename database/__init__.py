173027925b73d400a7c5775205c0e2f8471d6b35
Philippe Mathew <philmattdev@gmail.com>
2023-07-08 01:14:39 +0800

    feat: add GridFS

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,9 @@
 import os
 
+import gridfs
 from mongoclass import MongoClassClient
 
 mongoclass = MongoClassClient(os.environ["MONGODB_DB_NAME"], os.environ["MONGODB_URI"])
+
+db = mongoclass[os.environ["MONGODB_DB_NAME"]]
+fs = gridfs.GridFS(db)

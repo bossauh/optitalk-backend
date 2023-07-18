af371bdb671213992d10f18577cb7e58e4e37bf0
Philippe Mathew <philmattdev@gmail.com>
2023-07-18 14:21:42 +0800

    feat: don't render ad on mobile for now

--- frontend/src/routes/characters.tsx
+++ frontend/src/routes/characters.tsx
@@ -137,9 +137,11 @@ const Characters: FC = () => {
 
   return (
     <Container fluid mt="lg" mx="xs" pb="90px">
-      <Flex direction="column" align="center">
-        <AdComponent client="ca-pub-3336177471309301" slot="5172839273" format="autos" />
-      </Flex>
+      {!isSm && (
+        <Flex direction="column" align="center">
+          <AdComponent client="ca-pub-3336177471309301" slot="5172839273" format="autos" />
+        </Flex>
+      )}
       <ActiveCharacterItem />
       <CharactersSearchBox />
       <MediaQuery

4d51f669bba879e699814fc76a0e5af6ac00f056
Philippe Mathew <philmattdev@gmail.com>
2023-07-19 14:49:06 +0800

    refactor: remove unused imports

--- frontend/src/blogs/Changelog.tsx
+++ frontend/src/blogs/Changelog.tsx
@@ -1,14 +1,9 @@
-import { Accordion, Anchor, Box, Divider, Image, List, Stack, Text, Title } from "@mantine/core";
-import { modals } from "@mantine/modals";
+import { Divider, Stack } from "@mantine/core";
 import { FC } from "react";
-import { useNavigate } from "react-router-dom";
-import StoryMode from "./StoryMode";
 import Version18 from "./versions/Version18";
 import Version19 from "./versions/Version19";
 
 const Changelog: FC = () => {
-  const navigate = useNavigate();
-
   return (
     <Stack spacing="xl">
       <Version19 />
